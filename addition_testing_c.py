68dc439a23480abc8a8cfc3dc8d3390d553723ab
tobsen666 <supertobsen@gmail.com>
2023-12-15 12:44:03 +0100

    changed test script to be independent of dll

--- addition_testing_c.py
+++ addition_testing_c.py
@@ -1,19 +1,48 @@
-# test_addition.py
-import ctypes
-import os
+import importlib
+import cffi
 import pytest
-from pathlib import Path
 
-# Load the C library
-#lib_path = os.path.join(os.path.dirname(__file__), 'addition.dll')  # Change to 'addition.so' on Linux
-dll_path = str(Path(__file__).resolve().parent / 'addition.dll')
-addition = ctypes.CDLL(dll_path)
 
-def test_addition_case1():
-    result = addition.addition(2, 3)
-    assert result == 5, "Expected the sum of 2 and 3 to be 5"
+def load_and_compile(filename, includes=None):
+    try:
+        # Read C code from the external file
+        with open(filename + '.c') as c_file:
+            c_code = c_file.read()
 
-def test_addition_case2():
-    result = addition.addition(-1, 5)
-    assert result == 3, "Expected the sum of -1 and 5 to be 3"
+        # Read includes from the external header file
+        if includes is not None:
+            with open(includes) as includes_file:
+                includes_content = includes_file.read()
+        else:
+            includes_content = None
+
+        # Pass source code to CFFI
+        ffibuilder = cffi.FFI()
+
+        if includes_content:
+            ffibuilder.cdef(includes_content)
+
+        ffibuilder.set_source(filename + '_', c_code)
+        ffibuilder.compile()
+
+        # Import and return resulting module
+        module = importlib.import_module(filename + '_')
+        return module.lib
+
+    except Exception as e:
+        raise RuntimeError(f"Error loading and compiling {filename}: {str(e)}")
+
+
+def test_addition():
+    module = load_and_compile('add', includes='add.h')
+    result = module.add(1, 2)
+
+    # Custom logging
+    print(f"Test Result: {result}")
+
+    assert result == 1 + 2
+
+
+if __name__ == '__main__':
+    pytest.main(['-v', '--html=report.html'])
 
